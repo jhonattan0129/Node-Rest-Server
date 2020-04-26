afd7b382afff8f6f9712c09368d339578c52ac08
jhonattan perez <jrpm0129@gmail.com>
2020-04-26 14:54:06 -0500

    Heroku config

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://jrpm0129:bLIVXIwSslIeLd5C@cafe-6cctx.mongodb.net/test?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URL
 }
 
 process.env.URLDB = urlDB
