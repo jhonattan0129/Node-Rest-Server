5729698f477a2f33dcf864e23f4fd4f2031b63bd
jhonattan perez <jrpm0129@gmail.com>
2020-04-26 14:39:31 -0500

    Rest server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //puerto
 process.env.PORT = process.env.PORT || 3000
+
+//Entorno 
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//Base de datos, Local o en la Nube
+let urlDB
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://jrpm0129:bLIVXIwSslIeLd5C@cafe-6cctx.mongodb.net/test?retryWrites=true&w=majority'
+}
+
+process.env.URLDB = urlDB
--- server/server.js
+++ server/server.js
@@ -1,49 +1,28 @@
 require('./config/config')
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
-const bodyParser = require('body-parser')
 
-// parse application/x-www-form-urlencoded
+
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(require('./rutas/usuario'))
+    // parse application/x-www-form-urlencoded
+
 
 // parse application/json
 app.use(bodyParser.json())
 
 
-
-app.get('/usuario', function(req, res) {
-
-    res.json('Get Usuario')
-})
-
-app.post('/usuario', function(req, res) {
-    let body = req.body
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-
-})
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('Delete Usuario')
-})
-
-
+//conexion de BD
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
+        if (err) {
+            throw err
+        }
+        console.log('Base de datos ONLINE');
+    });
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando en puerto", process.env.PORT)
